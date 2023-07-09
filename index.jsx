e0c7c14051829106256be144de042f10e6ae9ede
Muhaemn <mwhaemnluqman2@gmal.com>
2023-07-10 02:23:29 +0300

    fixing base route

--- index.jsx
+++ index.jsx
@@ -19,7 +19,11 @@ import ErrorElement from "./pages/ErrorElement";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" errorElement={<ErrorElement />} element={<Layout />}>
+    <Route
+      path="/anime-app"
+      errorElement={<ErrorElement />}
+      element={<Layout />}
+    >
       <Route index loader={loaderHome} element={<Home />} />
       <Route path="animes" loader={loaderAnimes} element={<Animes />} />
       <Route path="mangas" loader={loaderMangas} element={<Mangas />} />
